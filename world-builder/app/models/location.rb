ec6a40f1f272a654cabc7d99df3688a2f68e3e83
Eric Backhaus <eric.backhaus@mnsu.edu>
2019-04-14 20:40:23 +0000

    Added image functionality to location and npc tables

--- world-builder/app/models/location.rb
+++ world-builder/app/models/location.rb
@@ -4,4 +4,6 @@ class Location < ActiveRecord::Base
     has_many :npcs
     has_many :parent_locations, :class_name => 'Connection'
     has_many :child_locations, :class_name => 'Connection'
+    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
