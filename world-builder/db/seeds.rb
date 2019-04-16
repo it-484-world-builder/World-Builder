fff457befa531c3ba4e52926b6b52457f1481527
Leo Caravello <jacob.caravello@mnsu.edu>
2019-04-16 04:31:50 +0000

    updated with saved and new seed file. Seed file is not working properly

--- world-builder/db/seeds.rb
+++ world-builder/db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-<<<<<<< HEAD
+
 more_worlds = [
     {:name => 'World 1', :description => 'cool world full of fun stuff'},
     {:name => 'World 2', :description => 'cooler world full of even more fun stuff'},
@@ -108,40 +108,4 @@ more_connections = [
 more_connections.each do |connections|
     Connection.create (connection)
 end
-=======
 
-
-more_campaigns = [
-  {:name => 'Jonny\'s Campaign', :world_id => 1, :user_id => 1},
-  {:name => 'Eric\'s Campaign', :world_id => 2, :user_id => 2},
-  {:name => 'Anthony\'s Campaign', :world_id => 1, :user_id => 3},
-  {:name => 'Leo\'s Campaign', :world_id => 1, :user_id => 4},
-  {:name => 'Mustafe\'s Campaign', :world_id => 2, :user_id => 5},
-]
-
-more_campaigns.each do |campaign|
-  Campaign.create!(campaign)
-  end
-  
-more_worlds = [
-  {:name => 'World 1', :description => 'cool world full of fun stuff'},
-  {:name => 'World 2', :description => 'cooler world full of even more fun stuff'},
-  {:name => 'World 3', :description => 'coolest world full of the most fun stuff'},
-]
-
-more_worlds.each do |world|
-  World.create!(world)
-  end
-  
-more_users = [
-  {:name => 'Jonny'},
-  {:name => 'Eric'},
-  {:name => 'Anthony'},
-  {:name => 'Leo'},
-  {:name => 'Mustafe'},
-]
-
-more_users.each do |user|
-  User.create!(user)
-  end
->>>>>>> 0c1183690009a08c0559180a20a7fb818c86f1f4
