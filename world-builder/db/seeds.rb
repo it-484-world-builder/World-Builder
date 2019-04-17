d4194f7346072dfbb20850121d7911e6b9caedf1
Leo Caravello <jacob.caravello@mnsu.edu>
2019-04-17 19:30:37 +0000

    Seed is working properly

--- world-builder/db/seeds.rb
+++ world-builder/db/seeds.rb
@@ -15,10 +15,10 @@ more_worlds = [
     
 more_worlds.each do |world|
     World.create!(world)
-end
+    end
 
 more_campaigns = [
-    {:name =>'Curse of Strahd',  :user_name =>'Dan', :world_name => 'Grey Hawk'},
+    {:name =>'Curse of Strahd',  :user_id => 8, :world_id => 4},
     {:name => 'Jonny\'s Campaign', :world_id => 1, :user_id => 1},
     {:name => 'Eric\'s Campaign', :world_id => 2, :user_id => 2},
     {:name => 'Anthony\'s Campaign', :world_id => 1, :user_id => 3},
@@ -29,7 +29,7 @@ more_campaigns = [
 
 more_campaigns.each do |campaign|
     Campaign.create!(campaign)
-end
+    end
 
 more_users = [
     {:name => 'Jonny'},
@@ -45,23 +45,23 @@ more_users = [
     
 more_users.each do |user|
     User.create!(user)    
-end
+    end
 
 more_characters = [
     { :name => 'Aaragon', :user_id =>7 },
-    { :name => 'Puff', :user_id =>8  }
+    { :name => 'Puff', :user_id =>6  }
 ]
 
 more_characters.each do |character|
     Character.create!(character)
-end
+    end
 
 =begin
-#commented out due to error
+#commented out due to error will be changed due to database
 
-more_character_campaigns = [
-    {:character =>'Aaragon', :campaign_name =>'Curse of Strahd'},
-    {:character =>'Puff', :campaign_name =>'Curse of Strahd'}
+more_user_campaigns = [
+    {:character_id =>'Aaragon', :campaign_id =>'Curse of Strahd'},
+    {:character_id=>'Puff', :campaign_id =>'Curse of Strahd'}
 
 ]
 
@@ -80,36 +80,37 @@ more_tags = [
     
 more_tags.each do |tag|
     Tag.create!(tag)
-end
+    end
 
 more_locations = [
-    { :name =>'Hidden Contient', :description => 'The location of this place is unknown', :gm_note => 'Was once a part of the contient, but disapeared when cursed', :player_note => 'Location is not know', :world_name => 'Grey Hawk' , :tag_rank => 1},
-    { :name =>'Hidden Country', :description => 'The location of this place is unknown', :gm_note => 'Was once a part of the contient, but disapeared when cursed', :player_note => 'Location is not know', :world_name => 'Grey Hawk' , :tag_rank => 2},
-    { :name =>'Barovia', :description => 'The land is surrounded by mist', :gm_note => 'This land is cursed because of Strhad ', :player_note => 'Place is scary', :world_name => 'Grey Hawk' , :tag_rank => 3},
-    { :name =>'Village of Barovia', :description => 'Small village of people', :gm_note => 'People are always gloomy and there is a haunted house', :player_note => 'Place is gloomy feeling', :world_name => 'Grey Hawk' , :tag_rank => 4},
-    { :name =>'Blood of the Vine Tavern', :description => 'Blood of the Vine Tavern a sign outside claims to be this location', :gm_note => 'three people work here', :player_note => 'There is a fire place inside', :world_name => 'Grey Hawk' , :tag_rank => 5}
+    { :name =>'Hidden Contient', :description => 'The location of this place is unknown', :gm_note => 'Was once a part of the contient, but disapeared when cursed', :player_note => 'Location is not know', :world_id => 4 , :tag_id => 1},
+    { :name =>'Hidden Country', :description => 'The location of this place is unknown', :gm_note => 'Was once a part of the contient, but disapeared when cursed', :player_note => 'Location is not know', :world_id => 4  , :tag_id => 2},
+    { :name =>'Barovia', :description => 'The land is surrounded by mist', :gm_note => 'This land is cursed because of Strhad ', :player_note => 'Place is scary', :world_id => 4 , :tag_id => 3},
+    { :name =>'Village of Barovia', :description => 'Small village of people', :gm_note => 'People are always gloomy and there is a haunted house', :player_note => 'Place is gloomy feeling', :world_id => 4  , :tag_id => 4},
+    { :name =>'Blood of the Vine Tavern', :description => 'Blood of the Vine Tavern a sign outside claims to be this location', :gm_note => 'three people work here', :player_note => 'There is a fire place inside', :world_id => 4  , :tag_id => 5}
     ]
  
  more_locations.each do |location|
-     Locations.create!(location)
- end
+    Location.create!(location)
+    end
  
  more_npcs = [
-     { :name => 'Ismark Kolyanovich', :description => 'Young man blonde grey hair and dark robes', :gm_note => 'called ismark the lesser because he lives in the shadow of his father', :player_note =>'Friendlier than most people in the village', :location_name =>' Blood of the Vine Tavern'}
+     { :name => 'Ismark Kolyanovich', :description => 'Young man blonde grey hair and dark robes', :gm_note => 'called ismark the lesser because he lives in the shadow of his father', :player_note =>'Friendlier than most people in the village', :location_id =>5}
      ]
      
 more_npcs.each do |npc|
     Npc.create!(npc)
-end
+    end
+
 
 more_connections = [
-    { :parent_location => 'Hidden Contient' , :child_location => 'Hidden Country'},
-    { :parent_location => 'Hidden Country' , :child_location => 'Barovia'},
-    { :parent_location => 'Barovia' , :child_location => 'Village of Barovia'},
-    { :parent_location => 'Village of Barovia' , :child_location => 'Blood of the Vine Tavern'}	
+    { :parent_location_id => 1 , :child_location_id => 2},
+    { :parent_location_id => 2 , :child_location_id => 3},
+    { :parent_location_id => 3 , :child_location_id => 4},
+    { :parent_location_id => 4 , :child_location_id => 5}	
     ]
     
 more_connections.each do |connection|
     Connection.create (connection)
-end
+    end
 
