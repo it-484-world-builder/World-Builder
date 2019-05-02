b985f2fcc8c28f38d944e6058c4a99693333457e
Jonathan Carlton <jonathan.carlton@mnsu.edu>
2019-05-02 05:15:31 +0000

    added functionality making hidden locations

--- world-builder/app/models/location.rb
+++ world-builder/app/models/location.rb
@@ -4,4 +4,23 @@ class Location < ActiveRecord::Base
     has_many :npcs
     has_many :parent_locations, :class_name => 'Connection'
     has_many :child_locations, :class_name => 'Connection'
+    
+        # setters and getters for tag foreign key
+    def tag_name=(name)
+        self.tag = Tag.find_or_create_by(name: name)
+    end
+
+    def tag_name
+        self.tag ? self.tag.name : nil
+    end
+    
+        # setters and getters for world foreign key
+    def world_name=(name)
+        self.world = World.find_or_create_by(name: name)
+    end
+
+    def world_name
+        self.world ? self.world.name : nil
+    end
+
 end
